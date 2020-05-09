4a7bd2c803a80d56e99ef20c346857184dec1b30
maximfreerider <ivanovma41@gmail.com>
2020-05-09 18:07:25 +0300

    Added ScrollView to the Home component

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,36 @@
 import React, {Component} from "react";
-import {View, Text} from "react-native"
+import {ScrollView, Text, View} from "react-native"
+import {Card} from 'react-native-elements'
+import {DISHES} from '../shared/dishes'
+import {PROMOTIONS} from '../shared/promotions'
+import {LEADERS} from '../shared/leaderas'
 
+function RenderItem(props) {
+    console.log(props)
+    const item = props.item;
+    if (item != null) {
+        return <Card
+            featuredTitle={item.name}
+            featuredSubtitle={item.designation}
+            image={require('./images/uthappizza.png')}
+            >
+            <Text style={{margin: 10}}>
+                {item.description}
+            </Text>
+        </Card>
+    } else {
+        return(<View></View>)
+    }
+}
 
 export class Home extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        }
     }
 
     static navigationOptions = {
@@ -13,9 +39,17 @@ export class Home extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component ! ! !</Text>
-            </View>
+            <ScrollView>
+                <RenderItem
+                    item={DISHES.filter(dish => dish.featured)[0]}
+                />
+                <RenderItem
+                    item={PROMOTIONS.filter(promo => promo.featured)[0]}
+                />
+                <RenderItem
+                    item={LEADERS.filter(leader => leader.featured)[0]}
+                />
+            </ScrollView>
         );
     }
 }
