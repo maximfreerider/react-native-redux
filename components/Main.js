d2a00e6d987e59e1e8ec77023a82f7429a695000
maximfreerider <ivanovma41@gmail.com>
2020-05-28 12:19:10 +0300

    Added network connection info to the Main component

--- components/Main.js
+++ components/Main.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
-import {View, Platform} from "react-native";
+import {View, Platform, ToastAndroid} from "react-native";
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders} from "../redux/ThunkCreators";
 import {connect} from "react-redux";
 import {MainNavigator} from "./MainDrawerNavigator/MainDrawerNavigator";
+import {addEventListener} from '@react-native-community/netinfo'
 
 const mapStateToProps = state => {
     return {}
@@ -18,20 +19,41 @@ const mapDispatchToProps = dispatch => {
 }
 
 class Main extends Component {
+    handleConnectivityChange = (connectionInfo) => {
+        switch (connectionInfo.type) {
+            case 'none':
+                ToastAndroid.show('You are offline ! ! !', ToastAndroid.LONG)
+                break
+            case 'wifi':
+                ToastAndroid.show('You are connected to WiFi ! ! !', ToastAndroid.LONG)
+                break
+            case 'cellular':
+                ToastAndroid.show('You are connected to Cellular ! ! !', ToastAndroid.LONG)
+                break
+            case 'unknown':
+                ToastAndroid.show('You have an unknown connection ! ! !', ToastAndroid.LONG)
+                break
+            default:
+                break
+        }
+    }
 
     componentDidMount() {
         this.props.fetchDishes()
         this.props.fetchComments()
         this.props.fetchPromos()
         this.props.fetchLeaders()
-    }
 
+        // addEventListener('connectionChange', this.handleConnectivityChange)
+    }
     render() {
         return (
-            <View style={{flex: 1, paddingTop: Platform.OS === 'ios'
+            <View style={{
+                flex: 1, paddingTop: Platform.OS === 'ios'
                     ? 0
-                    : Expo.Constants.statusBarHeight}}>
-                <MainNavigator />
+                    : Expo.Constants.statusBarHeight
+            }}>
+                <MainNavigator/>
             </View>
         )
     }
